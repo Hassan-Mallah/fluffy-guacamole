7f5767270183e282dcf7283d30b4506b350331c0
Hassan <hassansmallah@gmail.com>
2023-08-06 13:10:51 +0200

    playing Yahoo finance

--- yahoo-finanace.py
+++ yahoo-finanace.py
@@ -5,5 +5,23 @@ BTC_Ticker = yf.Ticker("BTC-USD")
 # type pandas.core.frame.DataFrame, data for 5 days
 BTC_Data = BTC_Ticker.history(period="5d")
 
-print('Ticket', BTC_Ticker.ticker)
+# print data
 print(BTC_Data.to_string())
+
+BTC_Data = BTC_Ticker.history(start="2023-07-17", end="2023-07-21", interval="15m")
+
+# print ticket
+print('Ticket', BTC_Ticker.ticker)
+
+
+# BTC_Data keys Index(['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'], dtype='object')
+
+# get one value
+print(BTC_Data['Open'][0])
+
+# get High
+print(BTC_Data['High'])
+
+# print rows
+for index, row in BTC_Data.iterrows():
+    print(index, row)
